838964120db093caa7b0eb07993175c43d14286d
SH <shzisun123@gmail.com>
2026-03-10 03:58:34 +0600

    module.ts fix

--- backend/src/admin/admin.module.ts
+++ backend/src/admin/admin.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AdminController } from './admin.controller';
-import { AdminService } from './admin.service';
+import { Module } from "@nestjs/common";
+import { AdminController } from "./admin.controller";
+import { AdminService } from "./admin.service";
+import { PrismaModule } from "../prisma/prisma.module"; // 🔥 PrismaModule ইমপোর্ট করুন
 
 @Module({
+  imports: [PrismaModule], // 🔥 এই লাইনটি অ্যাড করতে হবে
   controllers: [AdminController],
-  providers: [AdminService]
+  providers: [AdminService],
 })
 export class AdminModule {}
