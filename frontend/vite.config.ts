2ad84c16d92d3ac042f171103c63d27dcc395391
SH <shzisun123@gmail.com>
2026-03-12 05:13:35 +0600

    frontend initialize

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // 🔥 নতুন ইমপোর্ট
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        react() as any,
+        tailwindcss() as any, // 🔥 এখানে প্লাগইনটি অ্যাড করো
+    ],
 })
