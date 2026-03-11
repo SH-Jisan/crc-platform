6ab143c69012c18122cb35db12605acb4a3b2533
SH <shzisun123@gmail.com>
2026-03-12 04:24:28 +0600

    app.module.ts bug fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,8 +6,6 @@ import { PrismaModule } from "./prisma/prisma.module";
 import { EventsModule } from "./events/events.module";
 import { CampaignsModule } from "./campaigns/campaigns.module";
 import { DonationsModule } from "./donations/donations.module";
-import { GalleryController } from "./gallery/gallery.controller";
-import { GalleryService } from "./gallery/gallery.service";
 import { PostsModule } from "./posts/posts.module";
 import { GalleryModule } from "./gallery/gallery.module";
 import { SupabaseModule } from './supabase/supabase.module';
@@ -41,7 +39,7 @@ import { HealthModule } from './health/health.module';
     SearchModule,
     HealthModule,
   ],
-  controllers: [AppController, GalleryController],
-  providers: [AppService, GalleryService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
